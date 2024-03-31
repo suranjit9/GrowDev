767ea9f9c7d52f74a66a4352097ce87aef86c54d
Suranjit9 <suranjitm07@gmail.com>
2024-04-01 02:20:05 +0600

    emplement theme color

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,6 +1,8 @@
+
 import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
+ 
   return (
     <div className="h-screen">
       <h1>Home </h1>
---
+++ src/app/(root)/hello/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            dfghjkl
+        </div>
+    );
+};
+
+export default page;
